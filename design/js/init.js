cb8c0191b3edd130d13c62e9bb7615b69bf0d3c8
Timothy <timothy.gediga@outlook.com>
2018-01-12 16:03:29 +0100

    Added first version of the Design #3

---
+++ design/js/init.js
@@ -0,0 +1,12 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav({
+      menuWidth: 300, // Default is 300
+      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      draggable: true, // Choose whether you can drag to open on touch screens,    
+    });
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
