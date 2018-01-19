e6ac86519397e9007075e9083063bd64d9e8eab7
Timothy <timothy.gediga@outlook.com>
2018-01-19 11:12:32 +0100

    Minor changes at the design #3

--- design/js/init.js
+++ design/js/init.js
@@ -8,6 +8,32 @@
     });
     $('.parallax').parallax(); 
     $('ul.tabs').tabs();
+
+
     
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+$(document).ready(function() {
+
+  $("#login-toggle").click(function(){
+    if ($("#login-pane").is(":hidden")) {
+      $("#login-pane").slideDown("slow");
+    } else {
+      $("#login-pane").slideUp("slow");
+    }
+  });
+
+  $(document).mouseup(function(e) 
+  {
+      var container = $("#login-pane");
+  
+      // if the target of the click isn't the container nor a descendant of the container
+      if (!container.is(e.target) && container.has(e.target).length === 0) 
+      {
+        $("#login-pane").slideUp("slow");
+      }
+  });
+
+
+ });
