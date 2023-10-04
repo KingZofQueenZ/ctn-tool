a549827a1ea55d5c09cfb62610a25f7c1fa7e387
Timothy Gediga <timothy.gediga@zuehlke.com>
2023-10-04 18:17:12 +0200

    CTN-2: Added Information, Contact and Impressum

--- crossthenature-2/src/app/app-routing.module.ts
+++ crossthenature-2/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './content/home/home.component';
+import { InformationComponent } from './content/information/information.component';
+import { ContactComponent } from './content/contact/contact.component';
+import { ImpressumComponent } from './content/impressum/impressum.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
+  { path: 'information', component: InformationComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'impressum', component: ImpressumComponent },
 ];
 
 @NgModule({
--- crossthenature-2/src/app/app.module.ts
+++ crossthenature-2/src/app/app.module.ts
@@ -1,21 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { HomeComponent } from './content/home/home.component';
+import { MatButtonModule } from '@angular/material/button';
+import { ImpressumComponent } from './content/impressum/impressum.component';
+import { ContactComponent } from './content/contact/contact.component';
+import { InformationComponent } from './content/information/information.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ImpressumComponent,
+    ContactComponent,
+    InformationComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    FormsModule,
     MatSidenavModule,
     MatListModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
